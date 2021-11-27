f169ff70297b49e689c89b2f2d0c905f5d559c55
Sami Gråsten <sami.grasten@meltlake.com>
2021-11-27 17:21:53 +0200

    Temporal table query use cases

---
+++ Net6Demo/Models/Class.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Net6Demo.Models
+{
+    public class QueryingTemporalTable
+    {
+        private readonly ToDoDb _db;
+
+        public QueryingTemporalTable(ToDoDb db)
+        {
+            _db = db;
+        }
+
+        public List<string> ReturnItemsThatWereActiveBetweenTwoDates()
+        {
+            DateTime from = new DateTime(2021,1,1);
+            DateTime to = DateTime.Now;
+
+            return _db.ToDoItems
+                .TemporalFromTo(from, to)
+                .Select(x => x.Title)
+                .ToList();
+        }
+
+        public List<string> ReturnItemsOnGivenPointInTime()
+        {
+            DateTime on = new DateTime(2021,1,1);
+
+            return _db.ToDoItems
+                .TemporalAsOf(on)
+                .Select(x => x.Title)
+                .ToList();
+        }
+
+        public List<string> ReturnAllItemsOnHistory()
+        {
+            DateTime on = new DateTime(2021,1,1);
+
+            return _db.ToDoItems
+                .TemporalAll()
+                .Select(x => x.Title)
+                .ToList();
+        }
+
+    }
+}
