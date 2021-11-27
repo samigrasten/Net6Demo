1fc1f1ea8cb2bed2a57b238fecece6a3a6ee6d73
Sami Gråsten <sami.grasten@meltlake.com>
2021-11-27 16:14:09 +0200

    Added configuration for Entity Framework

---
+++ Net6Demo/Models/ToDoDb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+namespace Net6Demo.Models
+{
+    public class ToDoDb : DbContext
+    {
+        private readonly IConfiguration _options;
+
+        public ToDoDb(IConfiguration options)
+        {
+            _options = options;            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_options.GetValue<string>("ToDoConnectionstring"));            
+        }
+
+        public ISet<ToDoItem> ToDoItems { get; set; }
+    }
+}
--- Net6Demo/Program.cs
+++ Net6Demo/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Net6Demo;
 
@@ -7,12 +9,25 @@ builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+if (!string.IsNullOrEmpty(builder.Configuration["VaultUri"]))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri(builder.Configuration["VaultUri"]),
+        new DefaultAzureCredential());
+    var asd = builder.Configuration.GetValue<string>("ToDoConnectionstring");
+    builder.Services.AddDbContext<ToDoDb>();
+}
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<ToDoDb>();
+    db.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
