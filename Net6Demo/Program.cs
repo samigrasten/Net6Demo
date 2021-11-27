17aea59c74bd4ecc6a9b18e34d94813f33259aff
Sami Gråsten <sami.grasten@meltlake.com>
2021-11-27 12:31:17 +0200

    Added Items -api

--- Net6Demo/Program.cs
+++ Net6Demo/Program.cs
@@ -2,6 +2,8 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
@@ -20,4 +22,27 @@ app.UseEndpoints(endpoints =>
 
 app.MapGet("/", () => ".NET Conf Kuopio 2021");
 
+var items = new List<ToDoItem>
+{
+    new ToDoItem()
+    {
+        Id = 1,
+        Date = DateTime.Now,
+        Title = "Net Conf Kuopio",
+        Description = "Net Conf community event in Kuopio"
+    }
+};
+
+app.MapGet("/items", async () => items);
+
 app.Run();
+
+
+public class ToDoItem
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
+
